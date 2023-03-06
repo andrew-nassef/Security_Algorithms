618223b6804efde93bd225691c7c9ba014c549f3
Awabmohamed87 <62722055+Awabmohamed87@users.noreply.github.com>
2023-03-06 16:25:55 +0200

    V1.5
    Mono alphabetic analysis with frequency implemented.
    play fair decryption implemented.

--- SecurityPackage/securitylibrary/MainAlgorithms/Monoalphabetic.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/Monoalphabetic.cs
@@ -43,7 +43,7 @@ namespace SecurityLibrary
             for (int i = 0;i < emptyIndices.Length; i++)
                 answer[emptyIndices[i] - 65] = unMappedChars[i];
 
-            //return the final answer
+            //return the final key
             return toString(answer);
         }
 
@@ -107,9 +107,33 @@ namespace SecurityLibrary
         /// </summary>
         /// <param name="cipher"></param>
         /// <returns>Plain text</returns>
+        /// 
+        private List<char> getFrequenciesInfo() => new List<char>()
+            {
+                'E','T','A','O','I','N','S','R','H','L','D','C','U','M','F','P','G','W','Y','B','V','K','X','J','Q','Z'
+            };
+        private Dictionary<char, int>initializeDictionary() => new Dictionary<char, int>(){
+            {'A', 0 }, {'B', 0 }, { 'C', 0 }, { 'D', 0 }, {'E', 0 }, {'F', 0 }, {'G', 0}, {'H', 0 }, {'I', 0 },
+            {'J', 0 }, {'K', 0 }, {'L', 0 }, {'M', 0 }, {'N', 0 }, {'O', 0 }, {'P', 0 }, {'Q', 0 }, {'R', 0 },
+            {'S', 0 }, {'T', 0 }, {'U', 0 }, {'V', 0 }, {'W', 0 }, {'X', 0 }, {'Y', 0 }, {'Z', 0 }};
+      
         public string AnalyseUsingCharFrequency(string cipher)
         {
-            throw new NotImplementedException();
+
+            Dictionary<char, int> frequencies = initializeDictionary();
+
+            List<char> frequenciesInfo = getFrequenciesInfo();
+
+            for(int i = 0; i < cipher.Length; i++)frequencies[cipher[i]]++;
+            
+            frequencies = frequencies.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            
+            List<char> tmp = frequencies.Keys.ToList();
+            tmp.Reverse();
+
+            string plainText = "";
+            foreach (char c in cipher) plainText += frequenciesInfo[tmp.IndexOf(c)];
+            return  plainText.ToLower();
         }
     }
 }
--- math testing/math testing/Program.cs
+++ math testing/math testing/Program.cs
@@ -5,24 +5,56 @@
     {
         int[,] mat1 = new int[3, 4] { {1, 2,3, 4 },{5,6,7,8 },{9,10,11,12} };
         int[,] mat2 = new int[4, 3] { {1,2,3 },{4,5,6 },{7,8,9 },{10,11,12 } };
-        int[,] mat = MatrixMult3D(mat1, mat2);
-        Console.WriteLine(mat.GetLength(0));
-    }
+        int[,] mat = MatrixMult(mat1, mat2);
+        foreach (int i in mat)Console.WriteLine(i);
 
-    private static int[,] MatrixMult3D(int[,] matrix1, int[,] matrix2)
+        Console.WriteLine("----------------------------------------------------------");
+        int[,] mat3 = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+        int[,] mat4 = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+        int[,] mat0 = MatrixMult(mat3, mat4);
+        foreach (int i in mat0)Console.WriteLine(i);
+
+        int[,] x22 = new int[2, 2] { {5, 6 }, {8, 9 } };
+        int[,] x33 = new int[3, 3] { {8,5,3 }, {4,6,7 },{1,-1,20} };
+        int det1 = det(x22);
+        int det2 = det(x33);
+        Console.WriteLine(det2);
+    }
+        private static int[,] MatrixMult(int[,] matrix1, int[,] matrix2)
     {
         int[,] result = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
-        for (int a = 0; a < result.GetLength(0); a++)
+        for(int i = 0;i<result.GetLength(0);i++)
+            for(int j = 0;j<result.GetLength(1);j++)
+                result[i, j] = 0;
+        for(int i = 0; i <matrix1.GetLength(0); i++)
         {
-            for (int b = 0; b < result.GetLength(1); b++)
+            for(int j = 0; j <matrix1.GetLength(1); j++)
             {
-                    int res = 0;
-                for (int c = 0; c < matrix1.GetLength(0); c++)
+                for(int k = 0; k < matrix2.GetLength(1); k++)
                 {
-                    result[a, b] = res;
+                        result[i,k] += matrix1[i,j] * matrix2[j, k];
                 }
             }
         }
         return result;
+    }
+    private static int det(int[,] matrix)
+    {
+        if (matrix.GetLength(0) == 2)
+            return det2by2Matrix(matrix);
+        else
+            return det3by3Matrix(matrix);
+    }
+
+    private static int det3by3Matrix(int[,] matrix)
+    {
+        return matrix[0, 0] * det2by2Matrix(new int[2, 2] { { matrix[1, 1], matrix[1, 2] },{ matrix[2, 1], matrix[2, 2] } })
+            - matrix[0, 1] * det2by2Matrix(new int[2, 2] { { matrix[1, 0], matrix[1, 2] }, { matrix[2, 0], matrix[2, 2] } })
+            + matrix[0, 2] * det2by2Matrix(new int[2, 2] { { matrix[1, 0], matrix[1, 1] }, { matrix[2, 0], matrix[2, 1] } });
+    }
+
+    private static int det2by2Matrix(int[,] matrix)
+    {
+        return matrix[0,0] * matrix[1, 1] - matrix[0,1] * matrix[1, 0];
     }
 }
