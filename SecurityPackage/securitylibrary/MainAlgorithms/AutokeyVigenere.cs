0d5392e5635556ef392edc6f77e9bbbda29ee226
BassantAlaa-11 <78237166+BassantAlaa-11@users.noreply.github.com>
2023-03-07 17:23:45 +0200

    V1.6.8

--- SecurityPackage/securitylibrary/MainAlgorithms/AutokeyVigenere.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/AutokeyVigenere.cs
@@ -8,19 +8,92 @@ namespace SecurityLibrary
 {
     public class AutokeyVigenere : ICryptographicTechnique<string, string>
     {
+        private int mod(int det, int mod)
+        {
+            if (det >= 0) return det % mod;
+            else return mod - (det * -1 % mod);
+        }
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            plainText = plainText.ToUpper();
+
+            string key = "";
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                int shift = plainText[i] - 65;
+                int ans = mod(cipherText[i] - shift - 65, 26);
+                key += (char)(ans + 65);
+            }
+
+            string tmpKey = "";
+
+            int l = 0, matchCount = 0, stopIndex = key.Length;
+            for (int k = 1; k < key.Length; k++)
+            {
+                if (matchCount > 1) break;
+                if (key[k] == plainText[l])
+                {
+                    if (matchCount == 0)
+                        stopIndex = k;
+                    l++;
+                    matchCount++;
+                }
+                else
+                {
+                    matchCount = 0;
+                    l = 0;
+                }
+
+            }
+            for (int i = 0; i < stopIndex; i++)
+                tmpKey += key[i];
+            return tmpKey;
+            //throw new NotImplementedException();
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+
+            key = key.ToUpper();
+            int index = 0;
+            int shift = 0;
+            string plainText = "";
+            
+            for (int c = 0; c < cipherText.Length; c++)
+            {
+                index = cipherText[c] - 65;
+                shift = mod(index - (key[c] - 65), 26);
+                plainText += (char)(shift + 65);
+                key += (char)(shift + 65);
+
+            }
+            return plainText;
+            //throw new NotImplementedException();
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+
+            plainText = plainText.ToUpper();
+            key = key.ToUpper();
+
+            if (key.Length < plainText.Length)
+            {
+                int remain = plainText.Length - key.Length;
+                for (int i = 0; i < remain; i++)
+                {
+                    key += plainText[i];
+                }
+            }
+
+            string cipherText = "";
+            for (int p = 0; p < plainText.Length; p++)
+            {
+                int shift = plainText[p] - 65;
+                cipherText += (char)(((key[p] - 65 + shift) % 26) + 65);
+            }
+            return cipherText;
+            //throw new NotImplementedException();
         }
     }
 }
--- SecurityPackage/securitylibrary/MainAlgorithms/RailFence.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/RailFence.cs
@@ -8,19 +8,68 @@ namespace SecurityLibrary
 {
     public class RailFence : ICryptographicTechnique<string, int>
     {
+       
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+
+            plainText = plainText.ToUpper();
+            int depth1 = plainText.IndexOf(cipherText[1]) - plainText.IndexOf(cipherText[0]);
+            int depth2 = plainText.IndexOf(cipherText[3]) - plainText.IndexOf(cipherText[2]);
+            if(depth1 == depth2)
+            {
+                return depth1;
+            }
+            else
+            {
+                int depth3 = plainText.IndexOf(cipherText[5]) - plainText.IndexOf(cipherText[4]);
+                return depth3;
+            }
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            string plainText = "";
+            int z = cipherText.Length % key == 0 ? cipherText.Length / key : cipherText.Length / key + 1;
+
+             for (int k = 0; k < z; k++)
+                {
+                for (int p = 0; p < key; p++)
+                    {
+                        if ((p * z + k) < cipherText.Length)
+                        {
+                            plainText += cipherText[p * z + k];
+                        }
+                    }
+                    
+
+                }
+
+            
+            return plainText;
+            //throw new NotImplementedException();
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            string cipherText = "";
+            
+            for(int p = 0; p < key; p++)
+            {
+          
+                for (int k = 0; k < (plainText.Length % key == 0 ? plainText.Length / key : plainText .Length/ key + 1); k++)
+                {
+                    if ((p + k * key) < plainText.Length)
+                    {
+                        cipherText += plainText[p + k * key];
+                    }
+                   
+                }
+               
+            }
+            return cipherText;
+            
+
+            //throw new NotImplementedException();
         }
     }
 }
