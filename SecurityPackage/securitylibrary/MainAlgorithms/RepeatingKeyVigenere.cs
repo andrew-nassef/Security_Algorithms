7e159412dc26e00a6214ddaa8865f50e235b2b5c
Awabmohamed87 <62722055+Awabmohamed87@users.noreply.github.com>
2023-03-07 15:12:39 +0200

    V1.6.7
    Vignare auto key implemented.

--- SecurityPackage/securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -8,10 +8,46 @@ namespace SecurityLibrary
 {
     public class RepeatingkeyVigenere : ICryptographicTechnique<string, string>
     {
+        private int mod(int det, int mod)
+        {
+            if (det >= 0) return det % mod;
+            else return mod - (det * -1 % mod);
+        }
         public string Analyse(string plainText, string cipherText)
         {
+            plainText = plainText.ToUpper();
 
-            throw new NotImplementedException();
+            string key = "";
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                int shift = plainText[i] - 65;
+                int ans = mod(cipherText[i] - shift - 65, 26);
+                key += (char)(ans + 65);
+            }
+
+            string tmpKey = "";
+            
+            int l = 0, matchCount = 0, stopIndex = key.Length;
+            for (int k = 1; k <  key.Length; k++)
+            {
+                if (matchCount > 1) break;
+                if (key[k] == key[l])
+                {
+                    if (matchCount == 0)
+                        stopIndex = k;
+                    l++;
+                    matchCount++;
+                }
+                else
+                {
+                    matchCount = 0;
+                    l = 0;
+                }
+                
+            }
+            for(int i = 0;i<stopIndex;i++)
+                tmpKey += key[i];
+            return tmpKey;
         }
 
         public string Decrypt(string cipherText, string key)
@@ -20,25 +56,28 @@ namespace SecurityLibrary
             int index = 0;
             int shift = 0;
             string plainText = "";
-           for(int c = 0; c < cipherText.Length; c++)
+            if (key.Length < cipherText.Length)
+            {
+                int remain = cipherText.Length - key.Length;
+                for (int i = 0; i < remain; i++)
+                {
+                    key += key[i];
+                }
+            }
+            for (int c = 0; c < cipherText.Length; c++)
             {
                 index = cipherText[c] - 65;
-                shift = index - (key[c] - 65);
+                shift = mod(index - (key[c] - 65) , 26);
                 plainText += (char)(shift + 65);
 
             }
             return plainText;
-            //throw new NotImplementedException();
         }
 
         public string Encrypt(string plainText, string key)
         {
             plainText = plainText.ToUpper();
             key = key.ToUpper();
-            int shift = 0;
-            string cipherText = "";
-
-
 
             if (key.Length < plainText.Length)
             {
@@ -49,15 +88,13 @@ namespace SecurityLibrary
                 }
             }
 
-            for(int p = 0; p < plainText.Length; p++)
+            string cipherText = "";
+            for (int p = 0; p < plainText.Length; p++)
             {
-                shift = plainText[p] - 65;
+                int shift = plainText[p] - 65;
                 cipherText += (char)(((key[p] - 65 + shift)% 26) + 65);
             }
             return cipherText;
-               
-                
-            //throw new NotImplementedException();
         }
     }
 }
