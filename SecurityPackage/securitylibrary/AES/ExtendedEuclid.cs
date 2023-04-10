736186945431f2436fbc5d4559bd455039a2096f
Awabmohamed87 <62722055+Awabmohamed87@users.noreply.github.com>
2023-04-10 15:30:52 +0200

    V 2.3
    Extended eucledian implemented.

--- SecurityPackage/securitylibrary/AES/ExtendedEuclid.cs
+++ SecurityPackage/securitylibrary/AES/ExtendedEuclid.cs
@@ -16,7 +16,27 @@ namespace SecurityLibrary.AES
         /// <returns>Mul inverse, -1 if no det_neg_1</returns>
         public int GetMultiplicativeInverse(int number, int baseN)
         {
-            throw new NotImplementedException();
+            List<Round> rounds = new List<Round>
+            {
+                new Round(0, 1, 0, baseN, 0, 1, number)
+            };
+            while (true)
+            {
+                if (rounds.Last().B3 == 0)
+                    return -1;
+                else if(rounds.Last().B3 == 1) 
+                    return mod(rounds.Last().B2, baseN);
+
+                int Q = rounds.Last().A3 / rounds.Last().B3;
+                rounds.Add(new Round(Q,
+                    rounds.Last().B1,
+                    rounds.Last().B2,
+                    rounds.Last().B3,
+                    rounds.Last().A1 - Q * rounds.Last().B1,
+                    rounds.Last().A2 - Q * rounds.Last().B2,
+                    rounds.Last().A3 - Q *  rounds.Last().B3));
+            }
         }
+        private int mod(int n, int b) => n > 0 ? n : b - (n * -1 % b); 
     }
 }
---
+++ SecurityPackage/securitylibrary/AES/Round.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary.AES
+{
+    public class Round
+    {
+        public int q, A1, A2, A3, B1, B2, B3;
+        public Round(int q, int A1, int A2, int A3, int B1, int B2, int B3) 
+        {
+            this.q = q;
+            this.A1 = A1;
+            this.A2 = A2;
+            this.A3 = A3;
+            this.B1 = B1;
+            this.B2 = B2;
+            this.B3 = B3;
+        }
+    }
+}
