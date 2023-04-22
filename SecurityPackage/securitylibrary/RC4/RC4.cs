78d953756a8952262ce268e01099269c94ec45b2
Awabmohamed87 <62722055+Awabmohamed87@users.noreply.github.com>
2023-04-22 17:16:46 +0200

    RC4-V1.1

--- SecurityPackage/securitylibrary/RC4/RC4.cs
+++ SecurityPackage/securitylibrary/RC4/RC4.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,17 @@ namespace SecurityLibrary.RC4
     /// </summary>
     public class RC4 : CryptographicTechnique
     {
+        private void swap(ref int a, ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
          string HexaToBinary(string s) => Convert.ToString(Convert.ToInt64(s, 10), 2);
          string HexaToDecimal(string s) => Convert.ToString(Convert.ToInt64(s, 16), 10);
          string BinaryToDecimal (string s) => Convert.ToString(Convert.ToInt64(s, 2), 10);
 
-        string toHexa(string n) => Convert.ToInt64(n, 10).ToString("X");
+        string toHexa(string n) => Convert.ToInt64(n, 2).ToString("X");
         private string fixLength(string s, int length) => s.PadLeft(length, '0');
 
         private string XOR(string v1, string v2)
@@ -40,7 +47,7 @@ namespace SecurityLibrary.RC4
                     XOR_value += "0";
             }
 
-            return BinaryToDecimal(XOR_value);
+            return XOR_value;
 
         }
         public override string Decrypt(string cipherText, string key)
@@ -50,41 +57,41 @@ namespace SecurityLibrary.RC4
 
         public override  string Encrypt(string plainText, string key)
         {
-            string dec_key = HexaToDecimal(key);
-            plainText = HexaToDecimal(plainText);
-            int[] s_arr = new int[256];
-            int[] t_arr = new int[256];
+            const int length = 256;
+            string dec_key = key.Substring(2);
+            plainText = plainText.Substring(2);
+            int[] s_arr = new int[length];
+            int[] t_arr = new int[length];
             string result = "";
 
-            int j, tmp, ctr = 0,  ind_1 = 0, ind_2 = 0, key_idx, key_stream_byte;
-            for (int i = 0; i < 256; i++)
+            int j = 0, tmp, ctr = 0,  ind_1 = 0, ind_2 = 0, key_idx, key_stream_byte;
+            for (int i = 0; i < length; i++)
             {
                 s_arr[i] = i;
-                t_arr[i] = Convert.ToInt32(dec_key[ctr % dec_key.Length]);
+                t_arr[i] = dec_key[ctr % dec_key.Length] - 48;
                 ctr++;
             }
-            for (int i = 0; i < 256; i++)
+            for (int i = 0; i < length; i++)
             {
-                j = 0;
-                j = (j + s_arr[i] + t_arr[i]) % 256;
+                j = (j + s_arr[i] + t_arr[i]) % length;
                 tmp = s_arr[i];
                 s_arr[i] = s_arr[j];
                 s_arr[j] = tmp;
             }
+            int c = 0;
             for (int i = 0; i < plainText.Length; i++)
             {
-                ind_1 = (ind_1 + 1) % 256;
-                ind_2 = (ind_2 + s_arr[ind_1]) % 256;
+                ind_1 = (ind_1 + 1) % length;
+                ind_2 = (ind_2 + s_arr[ind_1]) % length;
                 tmp = s_arr[ind_1];
                 s_arr[ind_1] = s_arr[ind_2];
                 s_arr[ind_2] = tmp;
-                key_idx = (s_arr[ind_1] + s_arr[ind_2]) % 256;
+                key_idx = (s_arr[ind_1] + s_arr[ind_2]) % length;
                 key_stream_byte = s_arr[key_idx];
-                result += XOR(Convert.ToString(key_stream_byte), Convert.ToString(plainText[i]));
+                string ans = XOR(Convert.ToString(key_stream_byte), Convert.ToString(plainText[i]));
+                result += ans;
             }
             return toHexa(result);
-
-            //throw new NotImplementedException();
         }
     }
 }
