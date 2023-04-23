187109c136ef47fb1024a132fe7d50e0d9f75506
Awabmohamed87 <62722055+Awabmohamed87@users.noreply.github.com>
2023-04-23 17:41:06 +0200

    V 3.0
    RC4 and Diffie Helman implemented.

--- SecurityPackage/securitylibrary/DiffieHellman/DiffieHellman.cs
+++ SecurityPackage/securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -8,9 +8,23 @@ namespace SecurityLibrary.DiffieHellman
 {
     public class DiffieHellman 
     {
+        public int GeneratePublicKey(int alpha, int x, int q)
+        {
+            int key = 1;
+            while (x != 0)
+            {
+                key = (key * alpha) % q;
+                x--;
+
+            }
+            return key;
+        }
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+            List<int> secret_keys = new List<int>();
+            secret_keys.Add(GeneratePublicKey(GeneratePublicKey(alpha, xb, q), xa, q));
+            secret_keys.Add(GeneratePublicKey(GeneratePublicKey(alpha, xa, q), xb, q));
+            return secret_keys;
         }
     }
 }
--- SecurityPackage/securitylibrary/RC4/RC4.cs
+++ SecurityPackage/securitylibrary/RC4/RC4.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO.Pipes;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,23 +13,72 @@ namespace SecurityLibrary.RC4
     /// </summary>
     public class RC4 : CryptographicTechnique
     {
-        private void swap(ref int a, ref int b)
+        private void Swap(ref int a, ref int b) => (b, a) = (a, b);
+        
+        private void InitializeArrays(ref int[] S, ref int[] T,string key, int length)
         {
-            int tmp = a;
-            a = b;
-            b = tmp;
+            int ctr = 0;
+            for (int i = 0; i < length; i++)
+            {
+                S[i] = i;
+                T[i] = key[ctr];
+                ctr = (ctr + 1) % key.Length;
+            }
         }
-         string HexaToBinary(string s) => Convert.ToString(Convert.ToInt64(s, 10), 2);
-         string HexaToDecimal(string s) => Convert.ToString(Convert.ToInt64(s, 16), 10);
-         string BinaryToDecimal (string s) => Convert.ToString(Convert.ToInt64(s, 2), 10);
-
-        string toHexa(string n) => Convert.ToInt64(n, 2).ToString("X");
+        private int[] ApplyKSA(int[] S, int[] T, int length)
+        {
+            int j = 0;
+            for (int i = 0; i < length; i++)
+            {
+                j = (j + S[i] + T[i]) % length;
+                Swap(ref S[i], ref S[j]);
+            }
+            return S;
+        }
+        private int ApplyPRGA(ref int[] S, ref int ind_1, ref int ind_2, int length)
+        {
+            ind_1 = (ind_1 + 1) % length;
+            ind_2 = (ind_2 + S[ind_1]) % length;
+            Swap(ref S[ind_1], ref S[ind_2]);
+            int key_idx = (S[ind_1] + S[ind_2]) % length;
+            int key_stream_byte = S[key_idx];
+            return key_stream_byte;
+        }
+        private string ToString(string hexaText)
+        {
+            int fixedLength = hexaText.Length % 2 == 0 ? hexaText.Length / 2 : hexaText.Length / 2 + 1;
+            byte[] pla = new byte[fixedLength];
+            string result = "";
+            for (int i = 0; i < hexaText.Length; i += 2)
+            {
+                string s = "";
+                s += hexaText[i];
+                s += hexaText[i + 1];
+                pla[i / 2] = Convert.ToByte(s, 16);
+                result += (char)pla[i / 2];
+            }
+            return result;
+        }
+        private string toHex(string str)
+        {
+            byte[] ba = Encoding.Default.GetBytes(str);
+            string hexString = BitConverter.ToString(ba);
+            string modifiedHex = "0x";
+            for(int i = 0;i < hexString.Length;i ++) 
+            {
+                if (hexString[i] == '-')
+                    continue;
+                modifiedHex += hexString[i];
+            }
+            return modifiedHex;
+        }
+        private string DecimalToBinary(string s) => Convert.ToString(Convert.ToInt64(s, 10), 2);
         private string fixLength(string s, int length) => s.PadLeft(length, '0');
 
         private string XOR(string v1, string v2)
         {
-            v1 = HexaToBinary(v1);
-            v2 = HexaToBinary(v2);
+            v1 = DecimalToBinary(v1);
+            v2 = DecimalToBinary(v2);
 
             int fixedLength = Math.Max(v1.Length, v2.Length);
 
@@ -52,46 +102,69 @@ namespace SecurityLibrary.RC4
         }
         public override string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            string plainText = "";
+            bool isHexa = false;
+            if (cipherText.StartsWith("0x"))
+            {
+                key = ToString(key.Substring(2));
+                cipherText = ToString(cipherText.Substring(2));
+                isHexa = true;
+            }
+
+            const int length = 256;
+            int[] s_arr = new int[length];
+            int[] t_arr = new int[length];
+                
+            //initializing S and T
+            InitializeArrays(ref s_arr, ref t_arr, key, length);
+                
+            //Key-scheduling algorithm (KSA)
+            s_arr = ApplyKSA(s_arr, t_arr, length);
+
+            //Pseudo-random generation algorithm (PRGA)
+            int ind_1 = 0, ind_2 = 0;
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                int key_stream_byte = ApplyPRGA(ref s_arr, ref ind_1, ref ind_2, length);
+                string ans = XOR(Convert.ToString(key_stream_byte), Convert.ToString((int)cipherText[i]));
+                plainText += (char)Convert.ToInt32(ans, 2);
+            }
+
+            return isHexa ? toHex(plainText) : plainText;
         }
 
         public override  string Encrypt(string plainText, string key)
         {
+            string cipherText = "";
+
+            bool isHexa = false;
+            if (plainText.StartsWith("0x")) {
+
+                key = ToString(key.Substring(2));
+                plainText = ToString(plainText.Substring(2));
+                isHexa = true;
+            }
+                
             const int length = 256;
-            string dec_key = key.Substring(2);
-            plainText = plainText.Substring(2);
             int[] s_arr = new int[length];
             int[] t_arr = new int[length];
-            string result = "";
 
-            int j = 0, tmp, ctr = 0,  ind_1 = 0, ind_2 = 0, key_idx, key_stream_byte;
-            for (int i = 0; i < length; i++)
-            {
-                s_arr[i] = i;
-                t_arr[i] = dec_key[ctr % dec_key.Length] - 48;
-                ctr++;
-            }
-            for (int i = 0; i < length; i++)
-            {
-                j = (j + s_arr[i] + t_arr[i]) % length;
-                tmp = s_arr[i];
-                s_arr[i] = s_arr[j];
-                s_arr[j] = tmp;
-            }
-            int c = 0;
+            //initializing S and T
+            InitializeArrays(ref s_arr, ref t_arr, key, length);
+
+            //Key-scheduling algorithm (KSA)
+            s_arr = ApplyKSA(s_arr, t_arr, length);
+
+            //Pseudo-random generation algorithm (PRGA)
+            int ind_1 = 0, ind_2 = 0;
             for (int i = 0; i < plainText.Length; i++)
             {
-                ind_1 = (ind_1 + 1) % length;
-                ind_2 = (ind_2 + s_arr[ind_1]) % length;
-                tmp = s_arr[ind_1];
-                s_arr[ind_1] = s_arr[ind_2];
-                s_arr[ind_2] = tmp;
-                key_idx = (s_arr[ind_1] + s_arr[ind_2]) % length;
-                key_stream_byte = s_arr[key_idx];
-                string ans = XOR(Convert.ToString(key_stream_byte), Convert.ToString(plainText[i]));
-                result += ans;
+                int key_stream_byte = ApplyPRGA(ref s_arr, ref ind_1, ref ind_2, length);
+                string ans = XOR(Convert.ToString(key_stream_byte), Convert.ToString((int)plainText[i]));
+                cipherText += (char)Convert.ToInt32(ans, 2);
             }
-            return toHexa(result);
+            
+            return isHexa?toHex(cipherText) : cipherText;
         }
     }
 }
